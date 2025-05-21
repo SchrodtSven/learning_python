ceba488438d50d90963f983de0d1d7939eec0b56
Sven Schrodt <sven@schrodt.club>
2025-05-21 11:30:28 +0200

    Cleaning bot activated by PoEGptFoo

--- dotd/ds_jobs.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('data_science_job.csv')
-
-
-print(df.experience.value_counts(dropna=False))
--- dotd/wsgi.py
+++
@@ -1,6 +0,0 @@
-# 
-#
-# SEE: PEP 3333 – Python Web Server Gateway Interface v1.0.1
-# SEE: https://peps.python.org/pep-3333/
-
-
