b7ccba7d2d6a6f9f8bbf470e7183d0ac903627eb
Sven Schrodt <sven@schrodt.club>
2026-03-05 12:16:49 +0100

    Adjusting source code formatting

--- datascience/bmh_buta_2025.py
+++ datascience/bmh_buta_2025.py
@@ -1,35 +1,46 @@
+# Playing with statz of last (2025) Bundestagswahl (Election for German parliament)
+
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
-#cities = pd.read_csv('../dotd/cities.csv')
 
-election = pd.read_csv('kerg2.csv', sep=";")
-parties = election[election.Gruppenart=='Partei']
+# cities = pd.read_csv('../dotd/cities.csv')
+
+election = pd.read_csv("kerg2.csv", sep=";")
+parties = election[election.Gruppenart == "Partei"]
 all_votes = cum = parties.Anzahl.sum()
-cum = parties.groupby('Gruppenname').Anzahl.sum()/all_votes 
-party_names =  parties.Gruppenname.unique()
+cum = parties.groupby("Gruppenname").Anzahl.sum() / all_votes
+party_names = parties.Gruppenname.unique()
 
 selection = []
 print(party_names)
-cum.plot(title='Bundestag 2025', color=['blue', 'green', 'black', '#000011', 'pink', 'red', '#232323'], kind='bar')
+cum.plot(
+    title="Bundestag 2025",
+    color=["blue", "green", "black", "#000011", "pink", "red", "#232323"],
+    kind="bar",
+)
 
 
-#plt.show()
+# plt.show()
 print(cum.head())
 
 # exit()
 
-# all = election['Gültige Stimmen'].sum()
-# print(all)
-# elected = election.groupby('Gewählt')['Gültige Stimmen'].sum()/all
-# # elected['percentage'] = election.groupby('Gewählt')['Gültige Stimmen'].sum()/all
-# print(elected)
+all = election["Stimme"].sum()
+print(all)
+elected = election.groupby("Gewählt")["Stimme"].sum() / all
+# elected['percentage'] = election.groupby('Gewählt')['Gültige Stimmen'].sum()/all
+print(elected)
 
 
-# plt.style.use('bmh')
+plt.style.use("bmh")
 
 
-#elected.plot(title='Bundestag 2025', color=['blue', 'green', 'black', '#000011', 'pink', 'red', '#232323'], kind='bar')
+elected.plot(
+    title="Bundestag 2025",
+    color=["blue", "green", "black", "#000011", "pink", "red", "#232323"],
+    kind="bar",
+)
 
 
-#plt.show()
+plt.show()
--- datascience/dsj_agg.py
+++ datascience/dsj_agg.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv('data_science_salaries.csv')
+df = pd.read_csv("data_science_salaries.csv")
 print(df.head())
 
 print(df.company_location.value_counts())
